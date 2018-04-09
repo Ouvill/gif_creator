636167a3bec1425697c7d60719ff306da722a715
Ouvill <git@ouvill.net>
2018-04-09 16:46:18 +0900

    フォントサイズの調整を可能にした

--- public/javascripts/shi_gif.js
+++ public/javascripts/shi_gif.js
@@ -8,6 +8,9 @@ function save() {
     var textarea = document.getElementById("shi_textarea");
     var text = textarea.value;
 
+    var font_size = document.getElementById("font_size").value;
+    console.log(text);
+
     var post_url = "/api/create_gif";
 
     var xhr = new XMLHttpRequest();
@@ -15,7 +18,7 @@ function save() {
     xhr.setRequestHeader('content-type', 'application/json;charset=UTF-8');
     var json = JSON.stringify({
         "text": text,
-        "font_size": 42,
+        "font_size": font_size,
         "delay": 300,
         "repeat": 0
     });
@@ -26,6 +29,12 @@ function save() {
             var res = JSON.parse(xhr.responseText);
             console.log("res");
             console.dir(res);
+
+            var gif_diff = document.getElementById("gif_div");
+            while (gif_diff.firstChild) gif_diff.removeChild(gif_diff.firstChild);
+            var img = document.createElement("img");
+            img.setAttribute("src", res.url + '?' + new Date().getTime());
+            gif_diff.appendChild(img);
         }
     }
 }
