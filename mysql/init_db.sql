85309236a6448a802cf7951737636c5a69f8f762
Ouvill <git@ouvill.net>
2018-04-10 00:19:10 +0900

    データベースに記録

--- mysql/init_db.sql
+++ mysql/init_db.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS `flash_gif_maker_db` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
 USE flash_gif_maker_db
-CREATE TABLE `post_database` (
+CREATE TABLE `posts` (
   `post_id` int(11) NOT NULL AUTO_INCREMENT,
   `text` text DEFAULT NULL,
   `font_size` int(11) DEFAULT NULL,
