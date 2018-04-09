097aff88d3cc9cd9aa0279905ae18ab5a1b5e64c
Ouvill <git@ouvill.net>
2018-04-09 16:07:58 +0900

    セッションを利用してランダムなファイルを生成

--- routes/api/create_gif.js
+++ routes/api/create_gif.js
@@ -4,6 +4,10 @@ var gif = require('../../utils/create_gif');
 
 /* GET home page. */
 router.post('/', function (req, res, next) {
+    if (typeof req.session.user_id === 'undefined') {
+        console.log("set id");
+        req.session.user_id = getUniqueStr();
+    }
 
     var root_path = process.env.NODE_PATH
 
@@ -13,9 +17,10 @@ router.post('/', function (req, res, next) {
     var repeat = req.body.repeat;
     var width = 506;
     var height = 253;
-    var name = "sample.gif"
+    var name = req.session.user_id + ".png";
     var path = root_path + "/public/images/generate/" + name;
     var url = "/images/generate/" + name;
+
 
     gif.text_gif(path, text, font_size, delay, width, height, repeat);
 
@@ -23,3 +28,9 @@ router.post('/', function (req, res, next) {
 });
 
 module.exports = router;
+
+function getUniqueStr(myStrong) {
+    var strong = 1000;
+    if (myStrong) strong = myStrong;
+    return new Date().getTime().toString(16) + Math.floor(strong * Math.random()).toString(16)
+}
