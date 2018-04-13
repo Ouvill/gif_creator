f6edad3dc17a7baf47c18b7f04632f88c16373d4
Ouvill <git@ouvill.net>
2018-04-13 13:55:57 +0900

    画像データを登録

--- utils/img_list.js
+++ utils/img_list.js
@@ -3,10 +3,43 @@ var images = [
         name: "背景なし",
         filename: "white.png"
     },
+
     {
         name: "星空",
         filename: "jeremy-thomas-98201-unsplash.jpg"
     },
+    {
+        name: "ジャンプ",
+        filename: "SplitShire-04224.jpg"
+    },
+    {
+        name: "バラ",
+        filename: "SplitShire-6353.jpg"
+    },
+    {
+        name: "青空と雲",
+        filename:"sam-schooler-4824-unsplash.jpg"
+    },
+    {
+        name: "和紙/白1",
+        filename:"img180413120844.jpg",
+        url: "http://somephoto.net/item.php?i=fa053bbc9491b2b6538fd6c98192a43c"
+    },
+    {
+        name: "和紙/生成り色",
+        filename: "img180413121104.jpg",
+        url: "http://somephoto.net/item.php?i=94762be2e2e38f0ed40d6826cefc41e7"
+    },
+    {
+        name: "布/青色",
+        filename: "img180413121312.jpg",
+        url: "http://somephoto.net/item.php?i=70357d08efefee21b7d377fe279761b5"
+    },
+    {
+        name: "布/橙色",
+        filename: "img180413121529.jpg",
+        url: "http://somephoto.net/item.php?i=b5bf2603588b8750591eb9ba9f0180f7"
+    }
 ]
 
 module.exports = images;
