3fe200ddd73b3c5a64070299513db34ac204f36a
Ouvill <git@ouvill.net>
2018-04-13 13:55:19 +0900

    サムネイルの生成スクリプト修正

--- make_thumbnail.sh
+++ make_thumbnail.sh
@@ -4,7 +4,7 @@ CURRENT=$(cd $(dirname $0) && pwd)
 IMAGE_PATH="./public/images/backgrounds"
 DIST_PATH="./public/images/backgrounds/thumbnails"
 WIDTH=100
-HEIGH=100
+HEIGHT=100
 
 mkdir -p ${DIST_PATH}
 
@@ -14,5 +14,16 @@ IMAGE_LIST=$(ls *.jpg *.png)
 cd ${CURRENT}
 for IMAGE in ${IMAGE_LIST[@]}
 do
-    convert -thumbnail ${WIDTH}x${HEIGT}^ -gravity center -crop ${WIDTH}x${HEIGT}+0+0 ${IMAGE_PATH}/${IMAGE} ${DIST_PATH}/${IMAGE}
+    set $(identify -format "%w %h" ${IMAGE_PATH}/${IMAGE})
+    echo "width ${1}"
+    echo "height ${2}"
+    if [ ${1} -gt ${2} ]; then
+        convert -thumbnail x${HEIGHT} -gravity 'center' \
+            -crop ${WIDTH}x${HEIGHT}+0+0 \
+            ${IMAGE_PATH}/${IMAGE} ${DIST_PATH}/${IMAGE}
+    else
+        convert -thumbnail ${WIDTH}x -gravity 'center' \
+            -crop ${WIDTH}x${HEIGHT}+0+0 \
+            ${IMAGE_PATH}/${IMAGE} ${DIST_PATH}/${IMAGE}
+    fi
 done
