9e97a436092938f1b5f64ea632e86cdb9364cb1d
Ouvill <git@ouvill.net>
2018-04-23 14:21:12 +0900

    ファイルネームを指定

--- routes/api/upload_image.js
+++ routes/api/upload_image.js
@@ -1,12 +1,22 @@
 var express = require('express');
 var router = express.Router();
 var multer = require('multer');
-var upload = multer ({ dest: process.env.NODE_PATH  + '/public/images/uploads' })
+
+var storage = multer.diskStorage({
+    destination: process.env.NODE_PATH + '/public/images/uploads',
+    filename: function (req, file, cb) {
+        cb(null, req.cookies.user_id);
+    }
+})
+
+var upload = multer({ storage: storage})
 var set_id = require('../../utils/set_id');
 
-router.post('/', upload.single('original_image') ,function (req, res, next) {
+router.post('/', upload.single('original_image'), function (req, res, next) {
 
     let user_id = set_id(req, res);
+    console.log(req.file);
+
 
 });
 
